fdc287249b484db4f312a7c30b659faef56e6e71
Míriam Mengíbar Rodríguez <mirismr@correo.ugr.es>
2018-01-06 11:42:42 +0100

    fix rutas template #38

--- web.py
+++ web.py
@@ -12,19 +12,19 @@ app = Flask(__name__)
 @app.route("/")
 def raiz():
 	run('cd ~/proyectoIV17-18/')
-    return render_template('templates/base.html')
+	return render_template('templates/base.html')
 
 @app.route("/status")
 def status():
-    data = {"status": "OK"}
-    return json.dumps(data)
+	data = {"status": "OK"}
+	return json.dumps(data)
 
 @app.route("/hoy")
 def hoy():
 	run('cd ~/proyectoIV17-18/')
-    return render_template('templates/paginador.html')
+	return render_template('templates/paginador.html')
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+	port = int(os.environ.get('PORT', 80))
+	app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
